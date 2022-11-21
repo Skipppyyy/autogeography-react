04603395b2f4d10f04369eb7a603a7e514907689
Skippy <ethan.l.shen@gmail.com>
2022-11-21 08:05:37 -0800

    all pages added

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './contactstyles.css';
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className='container'>
+                <div className='top'> 
+                    <h1>Contact</h1>
+                </div>
+                <div className='bottom'>
+                    <button className='btn btn-dark'>Drive</button>
+                    <button className='btn'>Ride</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact; 
